143734cd65dbf099982177459a0450998e0b56de
Sergey Y <some@email>
2022-02-21 12:00:27 -0800

    Add typing annotations

--- chess/game/color.py
+++ chess/game/color.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from enum import Enum
 from enum import unique
 
@@ -7,5 +9,5 @@ class Color(Enum):
     WHITE = 1
     BLACK = 2
 
-    def opposite(self):
+    def opposite(self) -> Color:
         return Color.WHITE if self == Color.BLACK else Color.BLACK
--- chess/game/square.py
+++ chess/game/square.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from dataclasses import dataclass
 
 
@@ -6,13 +8,13 @@ class Square:
     file: int
     rank: int
 
-    def add_rank(self, steps: int):
+    def add_rank(self, steps: int) -> Square:
         return Square(file=self.file, rank=self.rank + steps)
 
-    def add_file(self, steps: int):
+    def add_file(self, steps: int) -> Square:
         return Square(file=self.file + steps, rank=self.rank)
 
-    def add_steps(self, file_steps, rank_steps):
+    def add_steps(self, file_steps, rank_steps) -> Square:
         return Square(file=self.file + file_steps, rank=self.rank + rank_steps)
 
     def __str__(self) -> str:
