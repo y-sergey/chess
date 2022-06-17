e8505c221922a3e5f3be12fb370d4dc1f9bd9ae8
Sergey Y <some@email>
2022-06-16 17:57:03 -0700

    Avoid creating unnecessary objects in Knight.threatens()

--- src/chess/game/knight.py
+++ src/chess/game/knight.py
@@ -19,7 +19,7 @@ class Knight(Piece):
     def threatens(self, src: Square, dst: Square, game_board) -> bool:
         rank_diff = abs(dst.rank - src.rank)
         file_diff = abs(dst.file - src.file)
-        return (rank_diff, file_diff) in [(1, 2), (2, 1)]
+        return (rank_diff == 1 and file_diff == 2) or (rank_diff == 1 and file_diff == 2)
 
     def get_available_moves(self, src: Square, game_board) -> List[Move]:
         return super()._get_available_moves_by_steps(src, game_board, Knight._MOVE_STEPS)
