1065b54f63b4556231bfb65b0c27c947e4918ed9
Sergey Y <some@email>
2022-06-16 17:59:14 -0700

    Fix Knight.threatens()

--- src/chess/game/knight.py
+++ src/chess/game/knight.py
@@ -19,7 +19,7 @@ class Knight(Piece):
     def threatens(self, src: Square, dst: Square, game_board) -> bool:
         rank_diff = abs(dst.rank - src.rank)
         file_diff = abs(dst.file - src.file)
-        return (rank_diff == 1 and file_diff == 2) or (rank_diff == 1 and file_diff == 2)
+        return (rank_diff == 1 and file_diff == 2) or (rank_diff == 2 and file_diff == 1)
 
     def get_available_moves(self, src: Square, game_board) -> List[Move]:
         return super()._get_available_moves_by_steps(src, game_board, Knight._MOVE_STEPS)
